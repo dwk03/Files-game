d99a8a51140afa8ea7df3ffd405a0b36d5895067
User1 <dwk.03fyongfly_proton@proton.me>
2026-02-27 20:33:33 +0700

    weird alphabets now in whaat

---
+++ whaat/whaat/weird alphabets/xyz/xyz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// khai báo hàm
+int xyz(int x, int y, int z)
+
+int main() {
+    int kq = xyz(int 6, 4, 9)
+    printf("%d", kq);
+    return 0;
+}
+
+// định nghĩa hàm
+int xyz(int x, int y, int z){
+    return a + b;
+}
